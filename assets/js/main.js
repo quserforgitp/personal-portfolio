49e8c1aba9677d7d9911557396071985cd55d7a5
quserforgitp <adealumnonegligas@gmail.com>
2024-04-30 09:57:13 -0600

    feat(index.html): :bento: added placeholder imgs
    global imgs for the project

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let hamburger = document.querySelector('#hamburger');
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+})()
+
+
