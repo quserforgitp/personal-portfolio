fc142941e64ab0d289bf38730a38a25b3678e00b
quserforgitp <adealumnonegligas@gmail.com>
2024-05-02 19:12:59 -0600

    chore(index.html): :construction: added js script to fold and unfold navbar
    the script toggles a class 'open' in hamburger button and the navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 (function () {
-    let navbar = document.querySelector('#navbar');
-    let hamburger = document.querySelector('#hamburger');
+    // HTML elements
+    const navbar = document.querySelector('.nav__list');
+    const hamburger = document.querySelector('.nav__hamburger-btn');
+
+    // Listeners
     hamburger.addEventListener('click', () => {
         navbar.classList.toggle('open');
+        hamburger.classList.toggle('open');
     });
 })()
 
