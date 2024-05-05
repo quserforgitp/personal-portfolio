25182e30240ee2a3637dc6c6d1c39db9dd481b07
quserforgitp <adealumnonegligas@gmail.com>
2024-05-05 11:25:02 -0600

    fix(JScripts): :sparkles: fixed navbar is working again !
    fixed names in querySelectors | variable and const names

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,14 +1,16 @@
 (function () {
-  const navbar = document.querySelector(".layout__nav");
+  /* DOM Elements */
+  const navbar = document.querySelector(".nav");
   const hamburgerBtn = document.querySelector(".nav__hamburger-btn");
-
-  const navlist = document.querySelector(".nav__list");
   const main = document.querySelector("main");
+
+  /* Variables */
   let navbarHeight = navbar.getBoundingClientRect().height;
   const breakpoint = main.offsetTop - navbarHeight;
   let windowPos;
   let isFixed = false;
 
+  /* Functions */
   function updatePos() {
     windowPos = window.scrollY;
   }
@@ -17,21 +19,22 @@
     updatePos();
 
     if (windowPos >= breakpoint && !isFixed) {
-      navlist.classList.remove("open");
+      navbar.classList.remove("open");
       hamburgerBtn.classList.remove("open");
 
-      navbar.classList.add("navbar-fixed");
+      navbar.classList.add("fixed");
       main.style.cssText = "margin-top: " + navbarHeight + "px;";
       isFixed = true;
     } else if (windowPos < breakpoint && isFixed) {
-      navbar.classList.remove("navbar-fixed");
+      navbar.classList.remove("fixed");
       main.style.cssText = "margin-top: " + 0;
       isFixed = false;
     } else if (windowPos >= breakpoint && isFixed) {
-      navlist.classList.remove("open");
+      navbar.classList.remove("open");
       hamburgerBtn.classList.remove("open");
     }
   }
 
+  /* Listeners */
   document.addEventListener("scroll", onScroll);
 })();
